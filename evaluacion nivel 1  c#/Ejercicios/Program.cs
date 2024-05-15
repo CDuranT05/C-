3381cff9fe3e9b6ee994b92fd475f382641165b0
Claudia <claudiadurantupano@gmail.com>
2024-05-15 14:40:16 -0400

    Área y perímetro

--- evaluacion nivel 1  c#/Ejercicios/Program.cs
+++ evaluacion nivel 1  c#/Ejercicios/Program.cs
@@ -44,13 +44,17 @@ Console.WriteLine("La multiplicacion de los numeros ingesados es:" + (numero1 * 
 Console.WriteLine("La division de los numeros ingesados es:" + (numero1 / numero2));
 
 
+//Ejercicio 3:
+Console.WriteLine("para calcular el rectangulo, ingresa la base y la altura");
 
+Console.WriteLine("ingresa la base:");
+int Base = int.Parse(Console.ReadLine());
+Console.WriteLine("ingresa la altura");
+int Altura = int.Parse(Console.ReadLine());
 
+int perimetro = (Base * 2) + (Altura * 2);
+Console.WriteLine("el perimetro es:" + perimetro);
 
-
-
-
-
-
-
+int area = Base * Altura;
+Console.WriteLine("el area es:" + area);
 
