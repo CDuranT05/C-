fc137772e9449d3df88ccf0634f719abb52e2bf2
Claudia <claudiadurantupano@gmail.com>
2024-05-15 14:38:53 -0400

    Calculadora simple

--- evaluacion nivel 1  c#/Ejercicios/Program.cs
+++ evaluacion nivel 1  c#/Ejercicios/Program.cs
@@ -30,7 +30,18 @@ if (imc < 18.5)
 Console.WriteLine("Tu índice de masa corporal (IMC) es:" + imc);
 Console.WriteLine("Estás:" + categoria);
  
+//Ejercicio 2:
 
+Console.WriteLine("por favor ingresa un numero:");
+int numero1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("por favor ingesa el segundo numero:");
+int numero2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("La suma de los numeros ingesados es:" + (numero1 + numero2));
+Console.WriteLine("La resta de los numeros ingesados es:" + (numero1 - numero2));
+Console.WriteLine("La multiplicacion de los numeros ingesados es:" + (numero1 * numero2));
+Console.WriteLine("La division de los numeros ingesados es:" + (numero1 / numero2));
 
 
 
